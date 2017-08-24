44729ec26a2aebfd051ba054083bb2fea8feeb92
c.stadler <c.stadler@adito.de>
2017-08-24 13:18:33 +0200

    #15383 Repository Bereitsstellung über Server => Java9-Modul-Optimierung

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module de.adito.jloadr {
   requires java.desktop;
   requires static jloadrLib;
   requires java.logging;
+  requires java.base;
 }
 
 /* Für Umstellung von jloadrLib auf Java9:   requires de.adito.jloadrLib; */
